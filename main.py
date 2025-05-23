0016a887c122bae224a28c1751a4ecad48196173
Duc Duy <nhanhm.mm16@gmail.com>
2025-05-23 16:57:45 +0700

    add test main

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from Model.CNN.extract_backboone import get_resnet101
 from torchvision import models
 from MoE.DenseMoE import DenseMoE
 from MoE.SpareMoE import MeOViT
+from DA.dann import DANN_MoE
+from DA.mcd import MCD
 
 if __name__ == "__main__":
     input_tensor = torch.randn(2, 3, 224, 224)  # Batch size 2, 3 kênh, 224x224
@@ -14,14 +16,24 @@ if __name__ == "__main__":
     # Mỗi expert là một MLP nhỏ
     experts = [resnet18_backbone for _ in range(4)]
 
-    model = DenseMoE(experts=experts, input_shape=input_shape)
-    output = model(input_tensor)
-    print(output.shape)
+    model_DenseMoE = DenseMoE(experts=experts, input_shape=input_shape)
+    output_DenseMoE = model_DenseMoE(input_tensor)
+    print(f"DenseMoE: {output_DenseMoE.shape}")
 
     # MeOViT
-    model = MeOViT(img_size=224, patch_size=7, in_channels=3,
+    model_MeOViT = MeOViT(img_size=224, patch_size=7, in_channels=3,
                    emb_dim=256, num_heads=4, num_layers=3, dropout=0.1,
                    num_classes=10, keep_ratio=0.5,
                    num_experts=4, top_k=2, expert_dim=128)
-    output = model(input_tensor)
-    print(output.shape)
+    output_MeOViT = model_MeOViT(input_tensor)
+    print(f"MeOViT: {output_MeOViT.shape}")
+
+    # DANN_MoE
+    model_DANN_MoE = DANN_MoE(feature_extractor=model_MeOViT, feat_dim=128, num_classes=10, grl_lambda=1.0)
+    output_DANN_MoE = model_DANN_MoE(input_tensor)
+    print(f"DANN_MoE: {output_DANN_MoE}")
+
+    # MCD
+    model_MCD = MCD(feature_extractor=model_MeOViT, feat_dim=128, num_classes=10)
+    output_MCD = model_MCD(input_tensor)
+    print(f"MCD: {output_MCD}")
