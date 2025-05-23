ee07538ac23bfa3901da81dfa4257a07cecb2e73
Duc Duy <nhanhm.mm16@gmail.com>
2025-05-23 16:04:25 +0700

    add dense moe

---
+++ MoE/DenseMoE.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+
+class DenseMoE(nn.Module):
+    def __init__(self,
+                 experts,               # List[Module]
+                 input_shape,           # Tuple like (C, H, W) or (features,)
+                 output_size=10):       # Output dim
+        super(DenseMoE, self).__init__()
+
+        self.experts = nn.ModuleList(experts)
+        self.num_experts = len(experts)
+
+        # Flatten all dims except batch
+        self.flatten = nn.Flatten(start_dim=1)
+
+        # Compute flattened input size
+        self.input_size = math.prod(input_shape)
+
+        # Gate network to produce expert weights
+        self.gate = nn.Sequential(
+            nn.Linear(self.input_size, self.num_experts),
+            nn.Softmax(dim=-1)
+        )
+
+    def forward(self, x):
+        # x shape: (B, C, H, W) or (B, Features)
+        x_flat = self.flatten(x)  # (B, input_size)
+
+        # (B, num_experts)
+        expert_weights = self.gate(x_flat)
+
+        # List of (B, output_size)
+        # (B, num_experts, output_size)
+        expert_outputs = torch.stack([expert(x)
+                                     for expert in self.experts], dim=1)
+
+        # Combine using weights: (B, output_size)
+        output = torch.sum(
+            expert_outputs * expert_weights.unsqueeze(-1), dim=1)
+
+        return output
--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import torch
 from Model.CNN.until import Backbone
 from Model.CNN.extract_backboone import get_resnet101
 from torchvision import models
-
+from MoE.DenseMoE import DenseMoE
 
 if __name__ == "__main__":
-    # resnet18 = models.resnet18(pretrained=True)
-
     input_tensor = torch.randn(2, 3, 224, 224)  # Batch size 2, 3 kênh, 224x224
-
+    input_shape = (3, 224, 224)
     resnet18_backbone = get_resnet101()
-    out_tensor = resnet18_backbone(input_tensor)
-    # Dự kiến: [2, 512]
-    print(f"ResNet18 output shape: {out_tensor.shape}")
+    # Mỗi expert là một MLP nhỏ
+    experts = [resnet18_backbone for _ in range(4)]
+
+    model = DenseMoE(experts=experts, input_shape=input_shape, output_size=10)
+    output = model(input_tensor)
+    print(output.shape)
