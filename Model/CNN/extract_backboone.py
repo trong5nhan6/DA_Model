4d9731e22fcc6f8002b21f7e3d0abbba0c1a239f
Duc Duy <nhanhm.mm16@gmail.com>
2025-05-24 08:38:27 +0700

    update backbone CNN

--- Model/CNN/extract_backboone.py
+++ Model/CNN/extract_backboone.py
@@ -3,31 +3,36 @@ from Model.CNN.until import Backbone
 from torchvision import models
 
 
-def get_resnet18(model=models.resnet18(pretrained=True), in_channels=3, img_size=(224, 224), output_dim=512):
+def get_resnet18(in_channels=3, img_size=(224, 224), output_dim=512):
+    model = models.resnet18(pretrained=True)
     resnet18_backbone = Backbone(
-        model=model, in_channels=3, img_size=(224, 224), output_dim=512)
+        model=model, in_channels=in_channels, img_size=img_size, output_dim=output_dim)
     return resnet18_backbone
 
 
-def get_resnet50(model=models.resnet50(pretrained=True), in_channels=3, img_size=(224, 224), output_dim=512):
+def get_resnet50(in_channels=3, img_size=(224, 224), output_dim=512):
+    model = models.resnet50(pretrained=True)
     resnet_backbone = Backbone(
-        model=model, in_channels=3, img_size=(224, 224), output_dim=512)
+        model=model, in_channels=in_channels, img_size=img_size, output_dim=output_dim)
     return resnet_backbone
 
 
-def get_resnet101(model=models.resnet101(pretrained=True), in_channels=3, img_size=(224, 224), output_dim=512):
+def get_resnet101(in_channels=3, img_size=(224, 224), output_dim=512):
+    model = models.resnet101(pretrained=True)
     resnet101_backbone = Backbone(
-        model=model, in_channels=3, img_size=(224, 224), output_dim=512)
+        model=model, in_channels=in_channels, img_size=img_size, output_dim=output_dim)
     return resnet101_backbone
 
 
-def get_densenet121(model=models.densenet121(pretrained=True), in_channels=3, img_size=(224, 224), output_dim=512):
+def get_densenet121(in_channels=3, img_size=(224, 224), output_dim=512):
+    model = models.densenet121(pretrained=True)
     densenet_backbone = Backbone(
-        model=model, in_channels=3, img_size=(224, 224), output_dim=512)
+        model=model, in_channels=in_channels, img_size=img_size, output_dim=output_dim)
     return densenet_backbone
 
 
-def get_densenet161(model=models.densenet161(pretrained=True), in_channels=3, img_size=(224, 224), output_dim=512):
+def get_densenet161(in_channels=3, img_size=(224, 224), output_dim=512):
+    model = models.densenet161(pretrained=True)
     densenet161_backbone = Backbone(
-        model=model, in_channels=3, img_size=(224, 224), output_dim=512)
+        model=model, in_channels=in_channels, img_size=img_size, output_dim=output_dim)
     return densenet161_backbone
