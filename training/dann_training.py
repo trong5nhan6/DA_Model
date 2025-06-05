65f0047f16b681ae2e09efdcf5ca35e982965370
Duc Duy <nhanhm.mm16@gmail.com>
2025-06-05 21:54:28 +0700

    update training dann, add mixup_alpha

--- training/dann_training.py
+++ training/dann_training.py
@@ -65,7 +65,7 @@ def evaluate(model, dataloader, device):
 
 def train_dann(model, source_loader, target_loader, source_test_loader, target_test_loader,
                device, epochs=10, lr=1e-3, step_size=5, gamma=0.5, beta=0.8, log_fn=None,
-               auxiliary_loss=False, use_mixup=False, mixup_alpha=0.2):
+               auxiliary_loss=False, use_mixup=False, mixup_alpha=0.4):
     """
     Train DANN (Domain Adaptation Neural Network) model
     Args:
